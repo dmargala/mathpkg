6c514e75e0f7ee2209077bd4bf0ac9ea8806c272
David Kirkby <dkirkby@uci.edu>
2013-01-09 09:36:53 -0800

    Add coverageContourPlot.

--- PlotTools.m
+++ PlotTools.m
@@ -1,4 +1,5 @@
 (* ::Package:: *)
+
 (* Created 5-Jan-2013 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu> *)
 
 BeginPackage["DeepZot`PlotTools`"]
@@ -18,6 +19,10 @@ The raster dimensions will be magnification*ImageSize and anti-aliasing will
 be performed using the specified oversampling factor."
 
 
+coverageContourPlot::usage=
+"Plots contours of the specified coverage fractions for a dataset."
+
+
 Begin["Private`"]
 
 
@@ -35,6 +40,29 @@ Rasterize[
 	Magnify[graphics,magnification],
 	ImageSize->magnification size,
 	RasterSize->oversampling magnification size]
+]
+
+
+coverageContourPlot[data_,{plotXmin_,plotXmax_,dx_},{plotYmin_,plotYmax_,dy_},fractions_:{0.6827,0.9545},plotOptions_:{}]:=
+Module[{kde,xmin,xmax,ymin,ymax,hist,sorted,cummulative,contourLevels},
+    (* Build a kernel density estimate of the PDF *)
+    kde=SmoothKernelDistribution[data];
+    (* Calculate the binning to use *)
+    xmin=Ceiling[Min[data[[;;,1]]]-dx,dx];
+    ymin=Ceiling[Min[data[[;;,2]]]-dy,dy];
+    xmax=Floor[Max[data[[;;,1]]]+dx,dx];
+    ymax=Floor[Max[data[[;;,2]]]+dy,dy];
+    (* Estimate the KDE integral over each bin *)
+    hist=Table[PDF[kde,{x,y}]dx dy,{x,xmin+dx/2,xmax-dx/2,dx},{y,ymin+dy/2,ymax-dy/2,dy}];
+    (* calculate the cummulative bin counts when bins are in descending contents order *)
+    sorted=Sort[Flatten[hist],Greater];
+    cummulative=Accumulate[sorted];
+    (* calculate the contour levels for each fraction *)
+    contourLevels=Map[sorted[[Position[cummulative,First[Nearest[cummulative,#1]],1,1][[1,1]]]]/(dx dy)&,fractions];
+    (* plot the contours *)
+    ContourPlot[PDF[kde,{x,y}],{x,plotXmin,plotXmax},{y,plotYmin,plotYmax},
+        plotOptions,Contours->contourLevels,PlotRange->{{plotXmin,plotXmax},{plotYmin,plotYmax},All},ContourShading->None
+    ]
 ]
 
 
