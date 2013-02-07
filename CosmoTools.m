385d667df3d6a82e62219d761a83493d187c50d6
David Kirkby <dkirkby@uci.edu>
2013-02-07 17:07:17 +0100

    Finish migration of original scope to use new createCosmology.

--- CosmoTools.m
+++ CosmoTools.m
@@ -21,25 +21,24 @@ radiationDensity::usage=
 in J/m^3 for a CMB temperature Tcmb in Kelvin and Nnu massless neutrinos."
 
 
-darkEnergyEvolution::usage=
-"darkEnergyEvolution[z,w0,wa] calcualtes the dark energy evolution with redshift (eqn 22 of Weinberg 2012)
-as a function of the equation of state parameters w0,wa."
-
-
 createCosmology::usage=
 "createCosmology[name] associates the following definitions with the symbol name:
  - \[CapitalOmega]rad[name]
- - hubbleFunction[name]
+ - \[CapitalOmega]de[name]
+ - \[CapitalOmega]mat[name]
+ - Hratio[name]
 Separate help is available for each of these definitions, e.g., ?\[CapitalOmega]rad.
 Use the following options to customize the cosmology that is created:
- - hValue (0.7) only matters if radiation is included.
+ - h (0.7) H0/(100 km/s/Mpc).
  - \[CapitalOmega]\[Phi] (0.73) present-day fraction of dark energy.
  - w0 (-1) dark-energy equation parameter state present-day value.
  - wa (0) dark-energy equation of state parameter derivative wrt scale a.
  - \[CapitalOmega]k (0) present-day curvature fraction.
  - Tcmb (2.725) present-day CMB temperature in Kelvin.
  - Nnu (3.046) effective number of massless neutrinos.
-To clear a previously defined cosmology, use Clear[name]."
+Use OptionValue[name,opt] to get option values associated with a cosmology.
+To clear a previously defined cosmology, use Clear[name]. Use the name provided
+here to identify the created cosmology in functions like comovingDistanceFunction."
 
 
 (* ::InheritFromParent:: *)
@@ -58,49 +57,41 @@ To clear a previously defined cosmology, use Clear[name]."
 "\[CapitalOmega]mat[name][z] returns the matter energy density relative to the critical density at the specified redshift."
 
 
-hubble::usage=
-"hubble[name][z] returns H(z)/H0."
+H0::usage=
+"H0[name] returns H0 in (km/s)/Mpc."
 
 
-hubbleFunction::usage=
-"Returns a function that evalutes H(z)/H(0) for the specified cosmology. Use the following
-options to customize the cosmology:
- - hValue (0.7) only matters if radiation is included.
- - \[CapitalOmega]\[Phi] (0.73) present-day fraction of dark energy.
- - w0 (-1) dark-energy equation parameter state present-day value.
- - wa (0) dark-energy equation of state parameter derivative wrt scale a.
- - \[CapitalOmega]k (0) present-day curvature fraction.
- - Tcmb (2.725) present-day CMB temperature in Kelvin.
- - Nnu (3.046) effective number of massless neutrinos."
+Hratio::usage=
+"Hratio[name][z] returns H(z)/H0."
 
 
 comovingDistanceFunction::usage=
-"Returns a function that evaluates the comoving distance to a redshift
-z <= zmax for the specified Hubble function. If hValue is specified,
-results are in Mpc, otherwise they are in Mpc/h."
+"comovingDistanceFunction[cosmology,zmax] returns a function that evaluates the comoving distance
+to a redshift z <= zmax for the named cosmology. Possible options are:
+ - physical (False) units are Mpc (True) or Mpc/h (False).
+ - pointsPerDecade (20) number of interpolation points to use per decade."
 
 
 angularDiameterDistanceFunction::usage
-"Returns a function that evaluates the comoving angular diameter distance to a redshift
-z <= zmax for the specified Hubble function and curvature \[CapitalOmega]k. If hValue is specified,
-results are in Mpc, otherwise they are in Mpc/h."
-
-
-lookbackTimeFunction::usage=
-"Returns a function that evaluates the lookback time to a redshift
-z <= zmax for the specified Hubble function. If hValue is specified,
-results are in Gyr, otherwise they are in Gyr/h."
-
-
-conformalTimeFunction::usage=
-"Returns a function that evaluates the conformal time at redshift z <= zmax
-for the specified Hubble function. If hValue is specified, results are in Gyr,
-otherwise they are in Gyr/h."
+"angularDiameterDistanceFunction[cosmology,zmax] returns a function that evaluates the comoving
+angular diameter distance to a redshift z <= zmax for the named cosmology. Options are the same
+as for comovingDistanceFunction."
 
 
 ageOfUniverse::usage=
-"Returns the age of the universe for the specified Hubble function. If hValue is specified,
-the result is in Gyr, otherwise it is in Gyr/h."
+"ageOfUniverse[cosmology] returns the age of the universe in Gyr for the named cosmology."
+
+
+lookbackTimeFunction::usage=
+"lookbackTimeFunction[cosmology,zmax] returns a function that evaluates the lookback time
+to a redshift z <= zmax for the named cosmology. Possible options are:
+ - physical (True) units Gyr (True) or Gyr/h (False).
+ - pointsPerDecade (20) number of interpolation points to use per decade."
+
+
+conformalTimeFunction::usage=
+"conformalTimeFunction[cosmology,zmax] returns a function that evaluates the conformal time
+at redshift z <= zmax for the named cosmology. Options are the same as for lookbackTimeFunction."
 
 
 Begin["Private`"]
@@ -123,14 +114,9 @@ Units`Convert[
 ]
 
 
-darkEnergyEvolution[z_,w0_,wa_]:=
-Exp[3(-((wa z)/(1 + z)) + (1 + w0 + wa) Log[1 + z])]
-
-
-Clear[createCosmology]
 createCosmology[name_,OptionsPattern[]]:=
 With[{
-    hValue=OptionValue["hValue"],
+    h=OptionValue["h"],
     \[CapitalOmega]\[CapitalLambda]=OptionValue["\[CapitalOmega]\[CapitalLambda]"],
     w0=OptionValue["w0"],
     wa=OptionValue["wa"],
@@ -138,38 +124,15 @@ With[{
     Tcmb=OptionValue["Tcmb"],
     Nnu=OptionValue["Nnu"]
 },
-    name/: \[CapitalOmega]rad[name]=Function[z,Evaluate[Simplify[radiationDensity[Tcmb,Nnu]/criticalDensityToday[hValue](1+z)^4]]];
+    name/: Options[name]= { "h"->h,"\[CapitalOmega]\[CapitalLambda]"->\[CapitalOmega]\[CapitalLambda],"w0"->w0,"wa"->wa,"\[CapitalOmega]k"->\[CapitalOmega]k,"Tcmb"->Tcmb,"Nnu"->Nnu };
+    name/: \[CapitalOmega]rad[name]=Function[z,Evaluate[Simplify[radiationDensity[Tcmb,Nnu]/criticalDensityToday[h](1+z)^4]]];
     name/: \[CapitalOmega]de[name]=Function[z,Evaluate[Simplify[\[CapitalOmega]\[CapitalLambda] Exp[3(-((wa z)/(1 + z)) + (1 + w0 + wa) Log[1 + z])]]]];
     name/: \[CapitalOmega]mat[name]=Function[z,Evaluate[Simplify[(1-\[CapitalOmega]de[name][0]-\[CapitalOmega]rad[name][0]-\[CapitalOmega]k)(1+z)^3]]];
-    name/: hubble[name]=Function[z,Evaluate[Sqrt[Simplify[\[CapitalOmega]de[name][z]+\[CapitalOmega]k (1+z)^2+\[CapitalOmega]mat[name][z]+\[CapitalOmega]rad[name][z]]]]];
+    name/: Hratio[name]=Function[z,Evaluate[Sqrt[Simplify[\[CapitalOmega]de[name][z]+\[CapitalOmega]k (1+z)^2+\[CapitalOmega]mat[name][z]+\[CapitalOmega]rad[name][z]]]]];
 ]
 SetAttributes[createCosmology,HoldFirst]
 Options[createCosmology]={
-    "hValue"->0.7,"\[CapitalOmega]\[CapitalLambda]"->0.73,"w0"->-1,"wa"->0,"\[CapitalOmega]k"->0,"Tcmb"->2.725,"Nnu"->3.046
-};
-
-
-hubbleFunction[options:OptionsPattern[]]:=
-With[{
-    hValue=OptionValue["hValue"],
-    \[CapitalOmega]\[CapitalLambda]=OptionValue["\[CapitalOmega]\[CapitalLambda]"],
-    w0=OptionValue["w0"],
-    wa=OptionValue["wa"],
-    \[CapitalOmega]k=OptionValue["\[CapitalOmega]k"],
-    Tcmb=OptionValue["Tcmb"],
-    Nnu=OptionValue["Nnu"]
-},
-Module[{
-    \[CapitalOmega]rad=radiationDensity[Tcmb,Nnu]/criticalDensityToday[hValue]
-},
-	Function[z,Evaluate[
-		With[{de=\[CapitalOmega]\[CapitalLambda] darkEnergyEvolution[z,w0,wa]},
-			Sqrt[Simplify[de+\[CapitalOmega]k (1+z)^2+(1-\[CapitalOmega]\[CapitalLambda]-\[CapitalOmega]rad-\[CapitalOmega]k)(1+z)^3+\[CapitalOmega]rad (1+z)^4]]
-		]
-	]]
-]]
-Options[hubbleFunction]={
-    "hValue"->0.7,"\[CapitalOmega]\[CapitalLambda]"->0.73,"w0"->-1,"wa"->0,"\[CapitalOmega]k"->0,"Tcmb"->2.725,"Nnu"->3.046
+    "h"->0.7,"\[CapitalOmega]\[CapitalLambda]"->0.73,"w0"->-1,"wa"->0,"\[CapitalOmega]k"->0,"Tcmb"->2.725,"Nnu"->3.046
 };
 
 
@@ -188,17 +151,19 @@ Module[{npts,ds,sval,partials,tabulated,interpolator},
 ]
 
 
+(* Returns numerator/H0 in the specified units *)
 hubbleScale[numerator_,hValue_,units_]:=
 Units`Convert[numerator/(100 hValue Units`Kilo Units`Meter/Units`Second/(Units`Mega Units`Parsec))/units,1]
 
 
-comovingDistanceFunction[hubble_,zmax_,options:OptionsPattern[]]:=
-With[{hValue=OptionValue["hValue"],pointsPerDecade=OptionValue["pointsPerDecade"]},
-Module[{scale},
-    scale=hubbleScale[PhysicalConstants`SpeedOfLight,hValue,Units`Mega Units`Parsec];
-	buildFunction[scale/hubble[#1]&,zmax,pointsPerDecade]
+comovingDistanceFunction[cosmology_,zmax_,options:OptionsPattern[]]:=
+With[{physical=OptionValue["physical"],pointsPerDecade=OptionValue["pointsPerDecade"]},
+Module[{h,scale},
+    h=If[physical===True,OptionValue[cosmology,"h"],1];
+    scale=hubbleScale[PhysicalConstants`SpeedOfLight,h,Units`Mega Units`Parsec];
+	buildFunction[scale/Hratio[cosmology][#1]&,zmax,pointsPerDecade]
 ]]
-Options[comovingDistanceFunction]={"hValue"->1,"pointsPerDecade"->20};
+Options[comovingDistanceFunction]={"physical"->False,"pointsPerDecade"->20};
 
 
 curvatureFunction[\[CapitalOmega]k_]:=Which[
@@ -207,42 +172,45 @@ curvatureFunction[\[CapitalOmega]k_]:=Which[
 True,Function[Dc,Dc]]
 
 
-angularDiameterDistanceFunction[hubble_,zmax_,\[CapitalOmega]k_,options:OptionsPattern[]]:=
-With[{hValue=OptionValue["hValue"],pointsPerDecade=OptionValue["pointsPerDecade"]},
-Module[{scale,func,curved},
-    scale=hubbleScale[PhysicalConstants`SpeedOfLight,hValue,Units`Mega Units`Parsec];
-	func=buildFunction[1/hubble[#1]&,zmax,pointsPerDecade];
-    curved=curvatureFunction[\[CapitalOmega]k];
+angularDiameterDistanceFunction[cosmology_,zmax_,options:OptionsPattern[]]:=
+With[{physical=OptionValue["physical"],pointsPerDecade=OptionValue["pointsPerDecade"]},
+Module[{h,scale,func,curved},
+    h=If[physical===True,OptionValue[cosmology,"h"],1];
+    scale=hubbleScale[PhysicalConstants`SpeedOfLight,h,Units`Mega Units`Parsec];
+	func=buildFunction[1/Hratio[cosmology][#1]&,zmax,pointsPerDecade];
+    curved=curvatureFunction[OptionValue[cosmology,"\[CapitalOmega]k"]];
     Function[z,scale curved[func[z]]]
 ]]
 Options[angularDiameterDistanceFunction]=Options[comovingDistanceFunction];
 
 
-lookbackTimeFunction[hubble_,zmax_,options:OptionsPattern[]]:=
-With[{hValue=OptionValue["hValue"],pointsPerDecade=OptionValue["pointsPerDecade"]},
+ageOfUniverse[cosmology_]:=
 Module[{scale},
-    scale=hubbleScale[1,hValue,Units`Giga Units`Year];
-	buildFunction[scale/(1+#1)/hubble[#1]&,zmax,pointsPerDecade]
-]]
-Options[lookbackTimeFunction]=Options[comovingDistanceFunction];
-
-
-ageOfUniverse[hubble_,hValue_:1]:=
-Module[{scale},
-    scale=hubbleScale[1,hValue,Units`Giga Units`Year];
-	scale NIntegrate[1/hubble[Exp[s]-1],{s,0,Infinity}]
+    scale=hubbleScale[1,OptionValue[cosmology,"h"],Units`Giga Units`Year];
+	scale NIntegrate[1/Hratio[cosmology][Exp[s]-1],{s,0,Infinity}]
 ]
 
 
-conformalTimeFunction[hubble_,zmax_,options:OptionsPattern[]]:=
-With[{hValue=OptionValue["hValue"],pointsPerDecade=OptionValue["pointsPerDecade"]},
-Module[{scale,eta0,func},
-    scale=hubbleScale[1,hValue,Units`Giga Units`Year];
-    eta0=scale NIntegrate[1/hubble[Exp[s]-1]Exp[s],{s,0,Infinity}];
-    func=buildFunction[scale/hubble[#1]&,zmax,pointsPerDecade];
+lookbackTimeFunction[cosmology_,zmax_,options:OptionsPattern[]]:=
+With[{physical=OptionValue["physical"],pointsPerDecade=OptionValue["pointsPerDecade"]},
+Module[{h,scale},
+    h=If[physical===True,OptionValue[cosmology,"h"],1];
+    scale=hubbleScale[1,h,Units`Giga Units`Year];
+	buildFunction[scale/(1+#1)/Hratio[cosmology][#1]&,zmax,pointsPerDecade]
+]]
+Options[lookbackTimeFunction]={"physical"->True,"pointsPerDecade"->20};
+
+
+conformalTimeFunction[cosmology_,zmax_,options:OptionsPattern[]]:=
+With[{physical=OptionValue["physical"],pointsPerDecade=OptionValue["pointsPerDecade"]},
+Module[{h,scale,eta0,func},
+    h=If[physical===True,OptionValue[cosmology,"h"],1];
+    scale=hubbleScale[1,h,Units`Giga Units`Year];
+    eta0=scale NIntegrate[1/Hratio[cosmology][Exp[s]-1]Exp[s],{s,0,Infinity}];
+    func=buildFunction[scale/Hratio[cosmology][#1]&,zmax,pointsPerDecade];
     Function[z,eta0-func[z]]
 ]]
-Options[conformalTimeFunction]=Options[comovingDistanceFunction];
+Options[conformalTimeFunction]=Options[lookbackTimeFunction];
 
 
 (*soundHorizonFunction[hubble_,zmax_,options:OptionsPattern[]]:=
