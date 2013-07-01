29cb9b81f3e7e606195a6c4417325106ff27cc8b
David Kirkby <dkirkby@uci.edu>
2013-07-01 07:26:28 +0200

    Protect against pointSpread divide by zero warning

--- BaoFitTools.m
+++ BaoFitTools.m
@@ -784,7 +784,7 @@ Module[{curves,points,pstyles,rvec,rpad,rmin,rmax,wgt,yvec,usepos,range,spread,l
   (* Determine the vertical range to use *)
   range=dataRange[yvec,FilterRules[{options},Options[dataRange]],"padFraction"->0.05];
   (* Apply horizontal point spreading *)
-  If[NumericQ[pointSpreadOption],
+  If[NumericQ[pointSpreadOption]&&Length[points]>1,
     rvec=points[[;;,;;,1]];
     spread=Range[-pointSpreadOption/2,+pointSpreadOption/2,
       pointSpreadOption/(Length[points]-1)];
